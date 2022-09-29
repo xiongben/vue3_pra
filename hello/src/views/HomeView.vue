c1b39a0803828f048c86156c0c2c8a42a42b3be7
kakaxi <kakaxi@gmail.com>
2022-09-29 11:24:18 +0800

    [add]

---
+++ hello/src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>hello, xb</h1>
+    <p>{{name}} {{age}}</p>
+    <p>{{job.type}} : {{job.salary}}</p>
+    <div @click="test1">test fn</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {ref, reactive} from "vue"
+
+export default {
+  name: 'HomeView',
+  setup () {
+    let name = ref("jack")
+    let age = ref(18)
+    let job = reactive({
+      type: 'web engineer',
+      salary: '30k'
+    })
+
+    function test1() {
+      name.value = "rose"
+      age.value = 25
+      job.salary = '70000'
+      job.type = 'java engineer'
+      console.log(name, age)
+    }
+
+    return {
+      name,
+      age,
+      job,
+      test1
+    }
+  }
+}
+</script>
