2cf72b8a4aa5fc7caa9e9678e3d0eb611a3e99c2
kakaxi <kakaxi@gmail.com>
2022-09-30 16:41:16 +0800

    [init]

---
+++ ts-practice-demo/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <p>{{messNum}}</p>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+  setup(){
+    let messNum = ref(20)
+
+    return {
+      messNum
+    }
+  }
+});
+</script>
