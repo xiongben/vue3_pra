f12afbcad2a8406203eda2cc936f80832aeb8137
kakaxi <kakaxi@gmail.com>
2022-10-02 21:49:53 +0800

    [add]

--- ts-practice-demo/src/views/HomeView.vue
+++ ts-practice-demo/src/views/HomeView.vue
@@ -36,7 +36,64 @@ type ReadOnlyInfo1 = ReadOnlyType<Info>
 
 type ReadOnlyInfo2 = Partial<Info>
 
+type PickInfo1 = Pick<Info, 'name'>
+type RecordInfo1 = Record<any, string>
 
+// 获取数组类型
+type typeTest1<T> = T[]
+type typeTest2 = typeTest1<string>[number]
+
+
+const recordTest1: RecordInfo1 = {223: 'dddd', 'ddd': 'ttt'}
+
+const pickinfo1: PickInfo1 = {
+  name: 'jack'
+}
+console.log(pickinfo1)
+
+// pick实现
+interface Info2{
+  name: string,
+  age: number,
+  address: string
+}
+
+const info5: Info2 = {
+  name: 'lison',
+  age: 18,
+  address: 'beijing'
+}
+
+function pick<T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K>{
+  let res:any = {}
+  keys.map(key => {
+    res[key] = obj[key]
+  })
+  return res
+}
+
+const nameAddress = pick(info5, ['name', 'age'])
+console.log(nameAddress)
+
+// record例子
+function mapObject<K extends string | number, T, U>(obj: Record<K, T>, f: (x: T) => U): Record<K, U>{
+  let res:any = {}
+  for(const key in obj) {
+    res[key] = f(obj[key])
+  }
+  return res
+}
+// unknown类型
+type type1 = string & unknown
+type type2 = unknown | string
+type type3 = never extends unknown ? true : false
+
+// infer
+type type4<T> = T extends any[] ? T[number] : T
+type Test2 = type4<string[]>
+
+type type5<T> = T extends Array<infer U> ? U : T
+type Test3 = type5<string[]>
 
 
 
