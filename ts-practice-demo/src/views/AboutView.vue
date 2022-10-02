c700c13c10cd6022b7ab06d005a38c8580e34d31
kakaxi <kakaxi@gmail.com>
2022-10-02 17:30:09 +0800

    [add]

--- ts-practice-demo/src/views/AboutView.vue
+++ ts-practice-demo/src/views/AboutView.vue
@@ -3,3 +3,133 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+class Person {
+  name: string;
+  age: number;
+  skill: string[];
+
+  constructor(name?: string, age?: number, skill?: string[]) {
+    this.name = name?name:""
+    this.age = age?age:0
+    this.skill = skill?skill:[]
+  }
+
+  introduce(): string {
+    return "my name is" + this.name + ",i'm " + this.age
+  }
+
+  moved(distance = 20){
+    console.log(`animal moved ${distance} m`)
+  }
+}
+
+interface animal {
+  name?: string,
+  age?: number,
+  [propName: string]: any
+}
+
+class Octopus {
+  readonly numberOfLegs: number = 8;
+  constructor(readonly name: string) {
+  }
+}
+
+
+abstract class Department {
+
+  constructor(public name: string) {
+  }
+
+  printName(): void {
+    console.log('Department name: ' + this.name);
+  }
+
+  abstract printMeeting(): void; // 必须在派生类中实现
+}
+
+class AccountingDepartment extends Department {
+
+  constructor() {
+    super('Accounting and Auditing'); // 在派生类的构造函数中必须调用 super()
+  }
+
+  printMeeting(): void {
+    console.log('The Accounting Department meets each Monday at 10am.');
+  }
+
+  generateReports(): void {
+    console.log('Generating accounting reports...');
+  }
+}
+
+interface GenericIdentityFn<T>{
+  (arg:T):T
+}
+
+function identity<T>(arg:T):T {
+  return arg;
+}
+
+let myIdentity:GenericIdentityFn<number> = identity
+
+function create1<T>(c:{new():T}):T {
+  return new c()
+}
+
+let person3 = create1(Person)
+console.log(person3)
+person3.moved(6666)
+
+export default defineComponent({
+  name: 'AboutView',
+  props: {
+    msg: String,
+  },
+  created() {
+    let name: string
+    let age: number
+    name = 'jack'
+    age = 18
+    console.log(`hello,my name is ${name}, age ${age}`)
+    let list1:number[]
+    list1 = [1,2,3,4]
+    let x:[number , string]
+    x = [34, 'hello']
+
+    enum Color {Red, Blue, Yellow}
+    let color1: Color
+    color1 = Color.Blue
+
+    let someVal = "eeerrrr"
+    let len1 = (someVal as string).length
+
+    let input = [1,2,3]
+    let [first, second, third] = input
+    let obj1 = {name: 'jack', age: 19, skill: 'soccer'}
+    let {name: name2, age: age2, skill} = obj1
+
+    let animal1:animal = {name: 'dog', age: 1}
+
+    let person2 = new Person()
+    person2.moved(999)
+
+    let octopus1 = new Octopus('tttt')
+    console.log(octopus1.name)
+
+    let department: AccountingDepartment;
+    department = new AccountingDepartment();
+    department.printName();
+    department.printMeeting();
+    department.generateReports();
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
--- ts-practice-demo/src/views/HomeView.vue
+++ ts-practice-demo/src/views/HomeView.vue
@@ -1,41 +1,43 @@
 <template>
   <div class="home">
-    <p>person: {{person1.name}}--{{person1.age}}</p>
-    <p>num1:{{num1}} -- mum2:{{num2}} -- res:{{resNum}}</p>
-    <p>{{messNum}} -- {{messNum2}}</p>
-    <p>res: {{res}}</p>
-    <button @click="addFn">add</button>
-    <button @click="printPerson">print person</button>
+
+    <button @click="aboutPage">to about page</button>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, reactive, toRef, toRefs } from 'vue';
+import { useRouter } from "vue-router"
 import type { Ref } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-
-class Person {
-  name: string;
-  age: number;
-  skill: string[];
-
-  constructor(name?: string, age?: number, skill?: string[]) {
-    this.name = name?name:""
-    this.age = age?age:0
-    this.skill = skill?skill:[]
-  }
-
-  introduce(): string {
-    return "my name is" + this.name + ",i'm " + this.age
-  }
+// 索引类型
+interface Objs<T>{
+  [key: string]: T
 }
 
-interface animal {
+const obj1: Objs<number> = {age: 18}
+let keys: Objs<number>['ttt'] = 888
+console.log(keys)
+
+//隐射类型
+interface Info {
+  age: number,
   name: string,
-  age: number
+  sex: string
 }
+
+type ReadOnlyType<T> = {
+  readonly [p in keyof T]: T[p]
+}
+
+type ReadOnlyInfo1 = ReadOnlyType<Info>
+
+type ReadOnlyInfo2 = Partial<Info>
+
+
+
 
 
 export default defineComponent({
@@ -44,32 +46,14 @@ export default defineComponent({
     HelloWorld,
   },
   created() {
-    let name: string
-    let age: number
-    name = 'jack'
-    age = 18
-    console.log(`hello,my name is ${name}, age ${age}`)
-    let list1:number[]
-    list1 = [1,2,3,4]
-    let x:[number , string]
-    x = [34, 'hello']
-
-    enum Color {Red, Blue, Yellow}
-    let color1: Color
-    color1 = Color.Blue
-
-    let someVal = "eeerrrr"
-    let len1 = (someVal as string).length
-
-    let input = [1,2,3]
-    let [first, second, third] = input
-    let obj1 = {name: 'jack', age: 19, skill: 'soccer'}
-    let {name: name2, age: age2, skill} = obj1
-
-    console.log(name2, age2)
+    console.log("=====create====")
+    // window.onmousedown = function(mouseEvent) {
+    //   console.log(mouseEvent);  //<- Error
+    // };
 
   },
   setup(){
+    const router = useRouter();
     let data = reactive({
       num1: 200,
       num2: 300,
@@ -80,17 +64,14 @@ export default defineComponent({
     let messNum2 = ref<number>(10)
     let res = ref<number>(0)
 
-    let person1 = reactive<Person>(new Person("jack", 19, ["football"]))
-
-    function printPerson(){
-      person1.name = "rose"
-      person1.age = 30
-      console.log(person1.name, person1.age)
-    }
-
     function addFn(){
       data.resNum = data.num1 + data.num2
       console.log(data.resNum)
+    }
+
+    function aboutPage(){
+      console.log(router)
+      router.push({path:'/about'})
     }
 
     return {
@@ -98,8 +79,7 @@ export default defineComponent({
       messNum,
       messNum2,
       addFn,
-      printPerson,
-      person1,
+      aboutPage,
       res
     }
   }
