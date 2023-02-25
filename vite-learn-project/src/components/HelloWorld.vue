ae80e36b6573522b5dd99b2339df1082487320e0
kakaixi <kakaxi@gmail.com>
2023-02-25 16:01:16 +0800

    [add]

--- vite-learn-project/src/components/HelloWorld.vue
+++ vite-learn-project/src/components/HelloWorld.vue
@@ -1,3 +1,10 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="bg-gray-200">
+    aaa
+  </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 import {skills} from '../common/common'
@@ -8,10 +15,6 @@ defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
-
-<template>
-  <h1>{{ msg }}</h1>
-</template>
 
 <style scoped>
 .read-the-docs {
