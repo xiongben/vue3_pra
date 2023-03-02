5643cc60163d27ca8b893e8f2e5425f345ed7fda
kakaixi <kakaxi@gmail.com>
2023-03-02 23:09:33 +0800

    [add]

--- vite-learn-project/src/components/CssDemo1.vue
+++ vite-learn-project/src/components/CssDemo1.vue
@@ -1,17 +1,50 @@
 <template>
   <h1>css demo</h1>
 <!--  flex -->
-  <div class="flex flex-row bg-amber-200 w-64 justify-between">
-    <div class="w-16 h-16 bg-blue-400">part 1</div>
-    <div class="w-16 h-16 bg-blue-400">part 2</div>
-    <div class="w-16 h-16 bg-blue-400">part 3</div>
+  <div class="flex flex-row">
+    <div class="flex flex-row bg-amber-200 w-64 justify-between">
+      <div class="w-16 h-16 bg-blue-400 text-sm font-bold leading-tight">part 1</div>
+      <div class="w-16 h-16 bg-blue-400 text-xb1 text-center">part 2</div>
+      <div class="w-16 h-16 bg-blue-400 text-base">part 3</div>
+    </div>
+
+    <div class="flex flex-col bg-amber-200 h-64 w-32 justify-between items-center">
+      <div class="w-16 h-16 bg-blue-400">part 1</div>
+      <div class="w-16 h-16 bg-blue-400">part 2</div>
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+    </div>
+
+    <div class="flex flex-row space-x-4">
+      <div class="w-12 h-12 bg-blue-400">1</div>
+      <div class="w-12 h-12 bg-blue-400">1</div>
+      <div class="w-12 h-12 bg-blue-400">1</div>
+    </div>
   </div>
 
-  <div class="flex flex-col bg-amber-200 h-64 w-32 justify-between items-center">
-    <div class="w-16 h-16 bg-blue-400">part 1</div>
-    <div class="w-16 h-16 bg-blue-400">part 2</div>
-    <div class="w-16 h-16 bg-blue-400">part 3</div>
+  <div class="flex flex-row">
+    <div class="bg-pink-50 grid grid-cols-3 gap-4 w-1/2">
+      <div class="bg-blue-400 p-4">part 3</div>
+      <div class="bg-blue-400 text-center capitalize">part 3</div>
+      <div class="bg-blue-400">part 3</div>
+      <div class="bg-blue-400">part 3</div>
+      <div class="bg-blue-400 h-14 truncate">part 3 sadfjkdsa sfhasdf sadfasdjf dsfhasdjhfads dsafhasdj fasjf sdfsd dsfsad sadfasdd sdf</div>
+      <div class="bg-blue-400 inline-block leading-10 h-16 text-red-500 text-center underline">
+        part 3
+        <span class="bg-amber-200 leading-10 text-xb1">999</span>
+      </div>
+    </div>
+
+    <div class="bg-pink-50 grid grid-cols-3 gap-2 w-1/2">
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+      <div class="w-12 h-12 bg-blue-400 place-self-center">part 3</div>
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+      <div class="w-16 h-16 bg-blue-400">part 3</div>
+    </div>
   </div>
+
+
 </template>
 
 <script setup lang="ts">
--- vite-learn-project/tailwind.config.cjs
+++ vite-learn-project/tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '0.9375rem',
+      'xb1': '1.125rem',
+    },
     extend: {},
   },
   plugins: [],
