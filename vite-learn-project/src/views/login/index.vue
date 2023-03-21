21a460eca59b48e3c9aafa0ab0b800b4a76d2e9e
kakaixi <kakaxi@gmail.com>
2023-03-21 23:09:51 +0800

    [add]

--- vite-learn-project/src/views/login/index.vue
+++ vite-learn-project/src/views/login/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="bg-blue-400 flex flex-col">
-    login
+  <div class="bg-blue-400">
+    <div class="flex-col flex items-center">
+      <div class="text-black font-bold">Login Page</div>
+      <input class="w-1/6 m-4"/>
+      <input class="w-1/6 m-4"/>
+      <div class="w-1/6 bg-amber-500 m-4 text-center">Login</div>
+    </div>
+    <div>
+
+    </div>
   </div>
 </template>
 
