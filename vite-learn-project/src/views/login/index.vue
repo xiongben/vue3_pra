a739e422431a579b0f2ccb899d6947dad03f7a32
kakaixi <kakaxi@gmail.com>
2023-03-24 22:59:23 +0800

    [add]

--- vite-learn-project/src/views/login/index.vue
+++ vite-learn-project/src/views/login/index.vue
@@ -4,19 +4,36 @@
       <div class="text-black font-bold">Login Page</div>
       <input class="w-1/3 m-4 h-8 rounded-md border-blue-600 border focus:text-blue-700 indent-2 focus:outline-none" placeholder="username" v-model="username"/>
       <input class="w-1/3 m-4 h-8 rounded-md border-blue-600 border focus:text-blue-700 indent-2 focus:outline-none" placeholder="password" v-model="pwd"/>
-      <div class="w-1/3 h-8 leading-8 bg-blue-600 m-4 text-center rounded-md text-white">Login</div>
+      <div class="w-1/3 h-8 leading-8 bg-blue-600 m-4 text-center rounded-md text-white" @click="login">Login</div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue";
+import {getLogin, UserResult} from "@/api/user";
+import {useUserStoreHook} from "@/store/modules/user";
+import {initRouter} from "@/router/utils";
 
 let username = ref<string>()
 let pwd = ref<string>()
 
 username.value = "yyyyy"
 pwd.value = "4455566"
+
+function login () {
+  const params = {
+    username: username,
+    password: pwd
+  }
+  useUserStoreHook().loginByUsername(params).then(res => {
+    if(res.success) {
+      initRouter().then(() => {
+        console.log("login success!")
+      })
+    }
+  })
+}
 </script>
 
 <style scoped>
