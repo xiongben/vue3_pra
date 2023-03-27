f35870d90b34b1aa2face491d9ecc6c73a4d9c9a
kakaixi <kakaxi@gmail.com>
2023-03-27 21:54:35 +0800

    [add]

--- vite-learn-project/src/router/utils.ts
+++ vite-learn-project/src/router/utils.ts
@@ -4,6 +4,7 @@ import {DataInfo, sessionKey} from "@/utils/auth";
 import {getAsyncRoutes} from "@/api/routes";
 import {usePermissionStoreHook} from "@/store/modules/permission";
 import router from "@/router/index";
+import {buildHierarchyTree} from "@/utils/tree";
 const modulesRoutes = import.meta.glob("/src/views/**/*.{vue,tsx}");
 
 
@@ -44,7 +45,39 @@ function handleAliveRoute(matched: RouteRecordNormalized[], mode?: string) {
  */
 function formatFlatteningRoutes(routesList: RouteRecordRaw[]) {
     if (routesList.length === 0) return routesList
+    let hierarchyList = buildHierarchyTree(routesList)
+    for(let i = 0; i < hierarchyList.length; i++) {
+        if (hierarchyList[i].children) {
+            hierarchyList = hierarchyList.slice(0, i+1).concat(hierarchyList[i].children, hierarchyList.slice(i+1))
+        }
+    }
+    return hierarchyList
+}
 
+/**
+ * 一维数组处理成多级嵌套数组（三级及以上的路由全部拍成二级，keep-alive 只支持到二级缓存）
+ * https://github.com/pure-admin/vue-pure-admin/issues/67
+ * @param routesList 处理后的一维路由菜单数组
+ * @returns 返回将一维数组重新处理成规定路由的格式
+ */
+function formatTwoStageRoutes(routesList: RouteRecordRaw[]) {
+    if (routesList.length === 0) return routesList
+    const newRoutesList: RouteRecordRaw[] = []
+    routesList.forEach((v: RouteRecordRaw) => {
+        if (v.path === "/") {
+            newRoutesList.push({
+                component: v.component,
+                name: v.name,
+                path: v.path,
+                redirect: v.redirect,
+                meta: v.meta,
+                children: []
+            })
+        } else {
+            newRoutesList[0]?.children?.push({...v})
+        }
+    })
+    return newRoutesList
 }
 
 /** 通过path获取父级路径 */
