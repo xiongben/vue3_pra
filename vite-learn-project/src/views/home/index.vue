e51e92f35c4e8880369f3ca9b9e7a100019cb199
kakaxi <kakaxi@gmail.com>
2023-04-08 00:25:22 +0800

    [add]

--- vite-learn-project/src/views/home/index.vue
+++ vite-learn-project/src/views/home/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-blue-400 flex flex-col">
     home demo page
+      <div class="w-1/6 h-8 leading-8 text-center bg-yellow-200 m-4 text-black">tabs page</div>
     <div class="w-1/6 h-8 leading-8 bg-blue-600 m-4 text-center rounded-md text-white" @click="logout">LogOut</div>
     <div class="w-1/6 h-8 leading-8 bg-blue-600 m-4 text-center rounded-md text-white" @click="tabsFn">tabs test</div>
   </div>
